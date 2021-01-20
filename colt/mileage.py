735cba82f46c235b57a110fd27fb9d7ff6f431cd
Chad <chad@chadoneil.com>
2021-01-20 13:31:11 -0500

    test

--- colt/mileage.py
+++ colt/mileage.py
@@ -3,6 +3,7 @@ kms = input() #get user input
 miles = float(kms)/1.60934 #convert from string to float and divide
 miles = round(miles, 2) #round the result
 print(f"Your {kms}km ride was {miles}mi ")  
+print("Hello there")
 
 # ROUND SYNTAX:
 # round(thing to round, how many decimal points)
