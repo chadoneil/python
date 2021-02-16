c51b969057d7c5eaf110c20eaccd814ada8c2f6e
Chad <chad@chadoneil.com>
2021-02-16 11:21:53 -0500

    update

--- EDx/test.py
+++ EDx/test.py
@@ -1,1 +1,8 @@
-print("Hello World")
+x = int(input("Enter an integer: "))
+if x%2 == 0:
+    print("ya")
+    print('Even')
+else:
+    print("nah")
+    print("odd")
+print("Done with conditional")
